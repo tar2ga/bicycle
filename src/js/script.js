ae65b782c87f986f178ca4a11350ce6237dc56b8
tar2ga <tar2ga@ya.ru>
2021-03-01 20:00:47 +0300

    31:05

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,28 @@
-function ibg() {
-	let ibg = document.querySelectorAll(".ibg");
+window.addEventListener('DOMContentLoaded', () => {
+	document.querySelector('.wrapper').classList.add('loaded');
+	//console.log(document.querySelector('.wrapper'));
 
-	for (var i = 0; i < ibg.length; i++) {
-		if (ibg[i].querySelector('img')) {
-			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+	const hamburger = document.querySelector('.icon-menu');
+	//console.log(hamburger);
+	hamburger.addEventListener('click', () => {
+		console.log('clicked');
+		//console.log(this);
+		//console.log(hamburger);
+		//document.querySelector('.icon-menu').classList.toogle('active');
+		document.querySelector('.menu__body').classList.toogle('active');
+		document.querySelector('.body').classList.toogle('lock');
+	})
+
+	function ibg() {
+		let ibg = document.querySelectorAll(".ibg");
+
+		for (var i = 0; i < ibg.length; i++) {
+			if (ibg[i].querySelector('img')) {
+				ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+			}
 		}
 	}
-}
 
-ibg();
+	ibg();
+
+});
